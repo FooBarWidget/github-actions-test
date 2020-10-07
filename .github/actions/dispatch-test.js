4171dcdaabf344bcf15f4a235749209e6d815aa8
Hongli Lai <honglilai@gmail.com>
2020-10-07 10:00:58 +0200

    test

--- .github/actions/dispatch-test.js
+++ .github/actions/dispatch-test.js
@@ -1,3 +1,3 @@
 const octokit = new Octokit();
 const url = `POST /repos/${process.env.GITHUB_REPOSITORY}/actions/workflows/test-test.yml/dispatches`;
-await octokit.request(url, { ref: process.env.GITHUB_REF });
+octokit.request(url, { ref: process.env.GITHUB_REF });
